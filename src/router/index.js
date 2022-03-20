8471a8a0ea2e6c3df6f33422e07ce5e05923744b
black-code1 <legrandmahamat1@gmail.com>
2022-03-20 12:35:52 +0100

    New Project Form

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import AddProjectView from "../views/AddProjectView.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/add",
+    name: "addProject",
+    component: AddProjectView,
   },
 ];
 
